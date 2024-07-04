e0ad36acca20409dcb468836f8bb60b04ec2e996
lucktosky726 <lucktosky@gmail.com>
2024-07-05 05:23:39 +0800

    fix:font

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,27 @@ function App() {
   return (
     <>
       <div>
+        <div className='flex flex-row p-5 '>
+          <div className='basis-1/4 px-20'>
+            <img src='/img/labiba.png'/>
+          </div>
+          <div className='basis-2/4 flex justify-center'>
+            <button className="btn btn-ghost">Ghost</button>
+            <button className="btn btn-ghost">AI Powers</button>
+            <button className="btn btn-ghost">Industries</button>
+            <button className="btn btn-ghost">Pricing</button>
+            <button className="btn btn-ghost">Careers</button>
+            <button className="btn btn-ghost">Contact</button>
+          </div>
+          <div className='basis-1/4 space-x-3'>
+            <select className="select select-ghost">
+              <option>En</option>
+              <option>AA</option>
+              <option>BB</option>
+            </select>
+            <button className="btn btn-outline btn-primary rounded-full">Free Trial</button>
+          </div>
+        </div>
         <div className='flex flex-row bg-gray-300 '>
           <div className='basis-1/2  pt-32 px-40'>
             <div className=' text-[37px] font-bold'>
@@ -15,13 +36,52 @@ function App() {
               your Customer<br/> 
               Experience?
              </div>
-             <div className=''>
+             <div className='pb-12'>
                <button className="btn btn-active btn-primary rounded-full">Learn more</button>
              </div>
           </div>
-          <div className='basis-1/2 bg-[url(img/circle.png)] bg-cover'>
+          <div className='basis-1/2 bg-[url(img/circle.png)] bg-cover relative'>
               {/* <img className='absolute right-[-80px]  top-[-300px] scale-125' src="img/circle.png"/> */}
-              <img className='' src="img/woman.png"/>
+              <div className='absolute z-10 w-96 top-36'>
+                <div className="chat chat-start">
+                  <div className="chat-image avatar">
+                    <div className="w-10 rounded-full bg-gray-100 px-3 py-2">
+                      <i class="fa-solid fa-robot text-primary"></i>
+                    </div>
+                  </div>
+                  <div className="chat-bubble bg-gray-100 text-black">
+                    Hello.. I'm your virtual assistant <br/>
+                    from BSF. How can I help you?
+                  </div>
+                </div>
+                <div className="chat chat-end">
+                  <div className="chat-image avatar">
+                    <div className="w-10 rounded-full bg-primary px-3 py-2">
+                     <i className='fa fa-user text-white'></i>
+                    </div>
+                  </div>
+                  <div className="chat-bubble chat-bubble-primary">Loans</div>
+                </div>
+                <div className="chat chat-start">
+                  <div className="chat-image avatar">
+                    <div className="w-10 rounded-full bg-gray-100 px-3 py-2">
+                      <i class="fa-solid fa-robot text-primary"></i>
+                    </div>
+                  </div>
+                  <div className="chat-bubble bg-gray-100 text-black">
+                    Which loan type do you seek?
+                  </div>
+                </div>
+                <div className="chat chat-end">
+                  <div className="chat-image avatar">
+                    <div className="w-10 rounded-full bg-primary px-3 py-2">
+                     <i className='fa fa-user text-white'></i>
+                    </div>
+                  </div>
+                  <div className="chat-bubble chat-bubble-primary">Personal loan</div>
+                </div>
+              </div>
+              <img className='absolute z-0 left-16' src="img/woman.png"/>
           </div>
         </div>
         <div className='bg-gray-100'>
@@ -201,14 +261,68 @@ function App() {
               <img src='/img/i1.png'/>
             </div>
             <div className='bg-white rounded-lg basis-1/4 p-20 flex justify-center'>
-              <img src='/img/i1.png'/>
+              <img src='/img/i2.png'/>
             </div>
             <div className='bg-white rounded-lg basis-1/4 p-20 flex justify-center'>
-              <img src='/img/i1.png'/>
+              <img src='/img/i3.png'/>
             </div>
             <div className='bg-white rounded-lg basis-1/4 p-20 flex justify-center'>
-              <img src='/img/i1.png'/>
+              <img src='/img/i4.png'/>
             </div>
+          </div>
+        </div>
+        <div className='bg-[url(img/4990.png)] bg-cover h-96'>
+          <div className='flex justify-center text-white text-4xl pt-32 text-center'>
+            Let’s create a great communication experience<br/> together!
+          </div>
+          <div className='flex justify-center pt-10'>
+           <button className="btn rounded-full text-white bg-gray-600/40">Learn more</button>
+          </div>
+        </div>
+        <div className='flex flex-row bg-black p-16 px-20 text-white'>
+          <div className='basis-1/4 pl-8'>
+            <div className='text-2xl'>Platform</div>
+            <div className='py-1'><a>Chatbots & Virtual Agents</a></div>
+            <div className='py-1'><a>Livecaht</a></div>
+            <div className='py-1'><a>Outbound</a></div>
+            <div className='py-1'><a>Omnichannel</a></div>
+            <div className='py-1'><a>CX Insights</a></div>
+            <div className='py-1'><a>More</a></div>
+            
+          </div>
+          <div className='basis-1/4 pl-8'>
+            <div className='text-2xl'>AI Powers</div>
+            <div className='py-1'><a>Natural Language Processing</a></div>
+            <div className='py-1'><a>Vision</a></div>
+            <div className='py-1'><a>Voice</a></div>
+            <div className='py-1'><a>Sentiment Analysis</a></div>
+            <div className='py-1'><a>Predictive Analytics</a></div>
+            <div className='py-1'><a>Language Generation</a></div>
+          </div>
+          <div className='basis-1/4 pl-8'>
+            <div className='text-2xl'>Industries</div>
+            <div className='py-1'><a>Banking & Financial Services</a></div>
+            <div className='py-1'><a>Telecom</a></div>
+            <div className='py-1'><a>Government Entities</a></div>
+            <div className='py-1'><a>Hospitality</a></div>
+            <div className='py-1'><a>E-Commerce</a></div>
+            <div className='py-1'><a>Logistics</a></div>
+          </div>
+          <div className='basis-1/4 pl-8'>
+            <div className='text-2xl'>Company</div>
+            <div className='py-1'><a>Pricing</a></div>
+            <div className='py-1'><a>Careers</a></div>
+            <div className='py-1'><a>Contact</a></div>
+            <div className='py-1'><a>Privacy Policy</a></div>
+            <div className='py-1'><a>Terms of Use</a></div>
+          </div>
+        </div>
+        <div className='bg-black text-white py-12 flex flex-row justify-around'>
+          <div className=''>
+              © 2024 Labiba for Artificial Intelligence LLC.
+          </div>
+          <div className=''>
+              © 2024 Labiba for Artificial Intelligence LLC.
           </div>
         </div>
       </div>
