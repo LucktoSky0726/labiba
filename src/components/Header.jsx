fb918b7648d68d0a0682227b15df39a348dfa1b3
top <atsumi000105@gmail.com>
2024-07-05 14:02:30 +0200

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-        <div className='flex flex-row p-5 '>
+        <div className='flex flex-row p-2 bg-white text-black'>
             <div className='basis-1/4 px-20'>
                 <img src='/img/labiba.png'/>
             </div>
             <div className='basis-2/4 flex justify-center'>
-                <button className="btn btn-ghost">Ghost</button>
+                <button className="btn btn-ghost">Platform</button>
                 <button className="btn btn-ghost">AI Powers</button>
                 <button className="btn btn-ghost">Industries</button>
                 <button className="btn btn-ghost">Pricing</button>
@@ -16,7 +16,7 @@ export default function Header() {
                 <button className="btn btn-ghost">Contact</button>
             </div>
             <div className='basis-1/4 space-x-3'>
-                <select className="select select-ghost">
+                <select className="select select-ghost bg-white border-white">
                 <option>En</option>
                 <option>FR</option>
                 <option>ES</option>
